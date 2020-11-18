c04b8c5683b058cbfd2a797c0b7f1815d88b39ff
Louis <louisdeveze78@gmail.com>
2020-11-18 14:02:00 +0100

    push

---
+++ Assets/Script/Data/MenuContext.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace NextOne
+{
+    public class MenuContext : MonoBehaviour
+    {
+        #region Managers
+        public GameObject uiManager;
+        public GameObject cameraManager;
+        #endregion
+    }
+}
---
+++ Assets/Script/StateMachine/Menu/MenuStateMachine.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NextOne
+{
+    class MenuStateMachine : StateMachine<MenuContext>
+    {
+
+    }
+
+}
