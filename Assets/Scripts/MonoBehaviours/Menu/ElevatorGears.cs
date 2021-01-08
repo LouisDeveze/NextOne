87837eca747a335cc89edf512843ac0f2a722d0b
Louis <louisdeveze78@gmail.com>
2021-01-08 12:55:10 +0100

    Elevator.Blend
    push elevator blend to share materials

---
+++ Assets/Scripts/MonoBehaviours/Menu/ElevatorGears.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NextOne
+{
+    /// <summary>
+    /// Elevator Gears update
+    /// </summary>
+    public class ElevatorGears : MonoBehaviour
+    {
+        public MenuContext context;
+
+        public static float speedFactor = -200;
+
+        private void Update()
+        {
+            float angle = context.elevatorSpeed * speedFactor * Time.deltaTime;
+            transform.Rotate(Vector3.up, angle);
+        }
+    }
+}
