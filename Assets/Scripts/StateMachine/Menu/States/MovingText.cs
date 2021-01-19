a13d452e3d684723db150a01201a1a080c3107d6
Kzenn <cfrancois189@gmail.com>
2021-01-19 10:14:28 +0100

    UI Manager
    Moving Text
    Icones

---
+++ Assets/Scripts/StateMachine/Menu/States/MovingText.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MovingText : MonoBehaviour
+{
+    public TextBehaviour TextPrefab;
+    float ms = 0;
+
+    TextBehaviour currentText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextPrefab.gameObject.SetActive(false);
+        currentText = Instantiate(TextPrefab,transform);
+        currentText.gameObject.SetActive(true);
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ms += Time.deltaTime;
+        if(ms > 5)
+        {
+            currentText = Instantiate(TextPrefab, transform);
+            ms = 0;
+            currentText.gameObject.SetActive(true);
+        }
+       
+
+    }
+}
---
+++ Assets/Scripts/StateMachine/Menu/States/TextBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBehaviour : MonoBehaviour
+{
+    public float TextWidth;
+    public float pixelsPerSecond;
+    RectTransform rt;
+
+    public float GetXPosition { get { return rt.position.x; }  }
+    public float GetWidth { get { return rt.rect.width; } }
+
+     void Awake()
+    {
+
+        rt = GetComponent<RectTransform>();
+        rt.position = new Vector3(Screen.width+rt.rect.width/2, rt.position.y, rt.position.z);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        rt.position += Vector3.left * pixelsPerSecond * Time.deltaTime;
+
+        if (GetXPosition <= -GetWidth)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
