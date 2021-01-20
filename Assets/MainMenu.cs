10f88cb31cd00189e774c3dd1f4d7d568d63541d
Kzenn <cfrancois189@gmail.com>
2021-01-20 16:43:16 +0100

    FromMenuToGame

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PlayGame()
+    {
+        Debug.Log(SceneManager.GetActiveScene().buildIndex);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1) ;
+    }
+}
