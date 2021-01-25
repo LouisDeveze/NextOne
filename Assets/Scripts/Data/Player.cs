ca936c201306294aacdaf207bf9411918fb24453
Kzenn <cfrancois189@gmail.com>
2021-01-26 00:03:57 +0100

    Save

---
+++ Assets/Scripts/Data/Player.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Assets.Scripts.Data
+{
+    public class Player : MonoBehaviour
+    {
+
+        public int season = 1;
+        public int episode = 5;
+        public int health = 150;
+        public int strength = 37;
+        public int charisma = 12;
+        public void SavePlayer()
+        {
+            SaveSystem.savePlayer(this);
+        }
+
+        public void LoadPlayer()
+        {
+            PlayerData data =  SaveSystem.LoadPlayer();
+            season = data.season;
+            episode = data.episode;
+            health = data.health;
+            strength = data.strength;
+            charisma = data.charisma;
+            Vector3 position;
+            position.x = data.position[0];
+            position.y = data.position[1];
+            position.z = data.position[2];
+
+            transform.position = position;
+        }
+    }  
+}
---
+++ Assets/Scripts/Data/PlayerData.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Data
+{
+    [System.Serializable]
+    public class PlayerData
+    {
+        public int season;
+        public int episode;
+        public int health;
+        public int strength;
+        public int charisma;
+        public float[] position;
+
+        public PlayerData(Player player)
+        {
+            season = player.season;
+            episode = player.episode;
+            health = player.health;
+            strength = player.strength;
+            charisma = player.charisma;
+
+            position = new float[3];
+
+            position[0] = player.transform.position.x;
+            position[1] = player.transform.position.y;
+            position[2] = player.transform.position.z;
+
+        }
+    }
+}
